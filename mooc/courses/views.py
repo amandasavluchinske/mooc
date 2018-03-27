697e3a92ae8880a4eb7977b7351d4ecb64f37b62
AMANDA <amandasavluchinske@gmail.com>
2018-03-27 15:34:02 -0300

    added course page

--- mooc/courses/views.py
+++ mooc/courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course, CourseManager
 from django.views.generic import *
 
@@ -14,3 +14,13 @@ class Courses(TemplateView):
         context = super().get_context_data(**kwargs)
         context['courses'] = Course.objects.all()
         return context
+
+class Details(DetailView):
+
+    template_name = 'details.html'
+    model = Course
+    context_object_name = 'course'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return (context)
--- mooc/mooc/urls.py
+++ mooc/mooc/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls import include, url  # noqa
 from django.contrib import admin
 from django.views.generic import TemplateView, FormView, ListView
-from courses.views import Courses
+from courses.views import Courses, Details
+from django.conf.urls.static import static
 
 import django_js_reverse.views
 
@@ -13,6 +14,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^cursos$', Courses.as_view(), name='courses'),
+    url(r'^cursos/(?P<pk>\d+)/$', Details.as_view(), name='details'),
 ]
 
 if settings.DEBUG:
@@ -20,3 +22,4 @@ if settings.DEBUG:
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
