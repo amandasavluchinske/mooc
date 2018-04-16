598e6ed93f3f71ad457b844e4c9ec873f5c1b53a
Amanda Savluchinske <amandasavluchinske@gmail.com>
2018-04-16 14:48:46 -0300

    courses are being displayed on dashboard

--- mooc/accounts/views.py
+++ mooc/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import auth
 from django.views.generic import *
 from django.contrib.auth.views import *
 from .forms import UserForm, EditAccount, PasswordReset, PasswordResetConfirm
-from courses.models import Course, CourseManager
+from courses.models import Course, CourseManager, Enrollments
 from users.models import User
 from django.views.generic.edit import FormMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -11,6 +12,7 @@ from templated_email.generic_views import TemplatedEmailFormViewMixin
 from templated_email import send_templated_mail
 from django.contrib.auth import REDIRECT_FIELD_NAME, get_user_model
 from django.contrib.auth.decorators import login_required
+
 
 # AUTHENTICATION
     
@@ -47,7 +49,6 @@ class Register(FormView):
         login(self.request, user)
         return redirect('home')
 
-# TODO - It's not working yet hehe
 class EditAccount(UpdateView, LoginRequiredMixin):
     template_name = 'edit.html'
     model = User
@@ -81,32 +82,6 @@ class PasswordReset(PasswordResetView):
         print(self)
         return super().form_valid(form)
 
-""" class PasswordResetConfirm(PasswordResetConfirmView):
-    template_name = 'registration/password_reset_confirm.html'
-    success_url = reverse_lazy('login')
-
-    def form_valid(self, form):
-        if not self.validlink:
-            return self.form_invalid(form)
-
-        user = self.save_form(form)
-
-        if INTERNAL_RESET_SESSION_TOKEN:
-            try:
-                del self.request.session[INTERNAL_RESET_SESSION_TOKEN]
-            except KeyError:
-                logger.info("Tried to delete password reset token from the session, "
-                            "but it couldn't be found. User: {}".format(str(user)))
-
-            if self.post_reset_login:
-                auth_login(self.request, user, self.post_reset_login_backend)
-
-            response = super(PasswordResetConfirmView, self).form_valid(form)
-            
-        return response """
-
-# WILL COME BACK TO THAT LATER
-
 class PasswordResetConfirm(PasswordResetConfirmView):
     template_name = 'registration/password_reset_confirm.html'
     success_url = reverse_lazy('dashboard')
@@ -120,6 +95,6 @@ class Dashboard(LoginRequiredMixin, TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['cursos'] = Course.objects.all()
+        context['enrollments'] = Enrollments.objects.filter(user=self.request.user)
         return (context)
 
