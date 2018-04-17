38988ad2d25a84bb8a61a94f0500de4cf92ae788
Amanda Savluchinske <amandasavluchinske@gmail.com>
2018-04-17 17:07:57 -0300

    working on custom template tags and saving

--- mooc/courses/templatetags/courses_tags.py
+++ mooc/courses/templatetags/courses_tags.py
@@ -1,11 +1,14 @@
-""" from django.template import Library
-
+from django import template
+from django.template import Library
+from courses.models import Enrollments
 register = Library()
 
-from .courses.models import Enrollments
-
-@register.inclusion_tag()
+@register.inclusion_tag('../templates/my_courses.html')
 def my_courses(user):
     enrollments = Enrollments.objects.filter(user=user)
     context = {'enrollments': enrollments}
-    return context """
+    return context
+
+@register.assignment_tag
+def load_my_courses(user):
+    return Enrollments.objects.filter(user=user)
